61f0ce4d4de0978405d175ddd6863523fd74e81a
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-17 16:24:56 +0000

    Initialized login page

---
+++ src/pages/login/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+function Login(){
+
+
+    return (
+        <>
+            <h1>Login Page</h1>
+        </>
+    )
+}
+
+
+export default Login;
