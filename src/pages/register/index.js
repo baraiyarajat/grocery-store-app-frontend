27b742fffde7538326a1dfc172d3b5c2afe177c4
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-19 10:56:24 +0000

    Added and Modified registration form fields

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -24,42 +24,32 @@ function Register(){
                                         <form>
                                             <div className="form-title"><h6>Sign Up</h6></div>
                                             <div className="form-group pos_rel">
-                                                <input id="full[name]" name="fullname" type="text" placeholder="Full name" className="form-control lgn_input" required=""/>
+                                                <input id="firstName" name="firstName" type="text" placeholder="First name" className="form-control lgn_input" required=""/>
                                                 <i className="uil uil-user-circle lgn_icon"></i>
                                             </div>
                                             <div className="form-group pos_rel">
-                                                <input id="email[address]" name="emailaddress" type="email" placeholder="Email Address" className="form-control lgn_input" required=""/>
+                                                <input id="lastName" name="lastName" type="text" placeholder="Last name" className="form-control lgn_input" required=""/>
+                                                <i className="uil uil-user-circle lgn_icon"></i>
+                                            </div>
+                                            <div className="form-group pos_rel">
+                                                <input id="email" name="email" type="email" placeholder="Email Address" className="form-control lgn_input" required=""/>
                                                 <i className="uil uil-envelope lgn_icon"></i>
                                             </div>
                                             <div className="form-group pos_rel">
-                                                <input id="phone[number]" name="phone" type="text" placeholder="Phone Number" className="form-control lgn_input" required=""/>
+                                                <input id="phone" name="phone" type="text" placeholder="Phone Number" className="form-control lgn_input" required=""/>
                                                 <i className="uil uil-mobile-android-alt lgn_icon"></i>
                                             </div>
+                                            
                                             <div className="form-group pos_rel">
-                                                <label className="control-label">Enter Code</label>
-                                                <ul className="code-alrt-inputs signup-code-list">
-                                                    <li>
-                                                        <input id="code[1]" name="number" type="text" placeholder="" className="form-control input-md" required=""/>
-                                                    </li>
-                                                    <li>
-                                                        <input id="code[2]" name="number" type="text" placeholder="" className="form-control input-md" required=""/>
-                                                    </li>
-                                                    <li>
-                                                        <input id="code[3]" name="number" type="text" placeholder="" className="form-control input-md" required=""/>
-                                                    </li>
-                                                    <li>
-                                                        <input id="code[4]" name="number" type="text" placeholder="" className="form-control input-md" required=""/>
-                                                    </li>
-                                                    <li>
-                                                        <a href="index.html" className="chck-btn hover-btn code-btn145" >Send</a>
-                                                    </li>
-                                                </ul>
-                                                <a href="index.html" className="resend-link">Resend Code</a>
-                                            </div>
-                                            <div className="form-group pos_rel">
-                                                <input id="password1" name="password1" type="password" placeholder="New Password" className="form-control lgn_input" required=""/>
+                                                <input id="password" name="password" type="password" placeholder="New Password" className="form-control lgn_input" required=""/>
                                                 <i className="uil uil-padlock lgn_icon"></i>
                                             </div>
+
+                                            <div className="form-group pos_rel">
+                                                <input id="confirmPassword" name="confirmPassword" type="password" placeholder="Confirm Password" className="form-control lgn_input" required=""/>
+                                                <i className="uil uil-padlock lgn_icon"></i>
+                                            </div>
+
                                             <button className="login-btn hover-btn" type="submit">Sign Up Now</button>
                                         </form>
                                     </div>
