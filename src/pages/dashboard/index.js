1b9b106c2298e18f2eb134274c376b31ae22153e
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-19 00:58:43 +0000

    Added dashboard template

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -38,7 +38,7 @@ function Dashboard(){
                                             <img src="images/avatar/img-5.jpg" alt=""/>
                                             <div className="img-add">
                                                 <input type="file" id="file"/>
-                                                <label for="file"><i className="uil uil-camera-plus"></i></label>
+                                                <label htmlFor="file"><i className="uil uil-camera-plus"></i></label>
                                             </div>
                                         </div>
                                         <h4>Johe Doe</h4>
@@ -60,7 +60,7 @@ function Dashboard(){
                                             <Link to="/rewards" className="user-item"><i className="uil uil-gift"></i>My Rewards</Link>
                                             <Link to="/wallet" className="user-item "><i className="uil uil-wallet"></i>My Wallet</Link>
                                             <Link to="/wishlist" className="user-item "><i className="uil uil-heart"></i>Shopping Wishlist</Link>
-                                            <Link to="/addresses" className="user-item"><i className="uil uil-location-point"></i>My Address</Link>
+                                            <Link to="/address" className="user-item"><i className="uil uil-location-point"></i>My Address</Link>
                                             <Link to="/logout" className="user-item"><i className="uil uil-exit"></i>Logout</Link>
                                         </div>
                                     </div>
