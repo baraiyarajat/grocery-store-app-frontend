10603694d02cfdb2466596b3ca11715d69a35295
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-11-19 00:56:53 +0000

    Added wishlist template

--- src/pages/wishlist/index.js
+++ src/pages/wishlist/index.js
@@ -37,7 +37,7 @@ function Wishlist(){
                                             <img src="images/avatar/img-5.jpg" alt=""/>
                                             <div className="img-add">													
                                                 <input type="file" id="file"/>
-                                                <label for="file"><i className="uil uil-camera-plus"></i></label>
+                                                <label htmlFor="file"><i className="uil uil-camera-plus"></i></label>
                                             </div>
                                         </div>
                                         <h4>Johe Doe</h4>
@@ -59,7 +59,7 @@ function Wishlist(){
                                             <Link to="/rewards" className="user-item"><i className="uil uil-gift"></i>My Rewards</Link>
                                             <Link to="/wallet" className="user-item "><i className="uil uil-wallet"></i>My Wallet</Link>
                                             <Link to="/wishlist" className="user-item active"><i className="uil uil-heart"></i>Shopping Wishlist</Link>
-                                            <Link to="/addresses" className="user-item"><i className="uil uil-location-point"></i>My Address</Link>
+                                            <Link to="/address" className="user-item"><i className="uil uil-location-point"></i>My Address</Link>
                                             <Link to="/logout" className="user-item"><i className="uil uil-exit"></i>Logout</Link>
                                         </div>
                                     </div>
