6f5b39ee31287f5b82e57a45b09d134d79f2bc92
Rajat Baraiya <baraiya.rajat@gmail.com>
2022-12-05 19:11:45 +0000

    Added Product Image and Add/Remove  from cart func

--- src/pages/singleProduct/index.js
+++ src/pages/singleProduct/index.js
@@ -2,25 +2,47 @@ import { useParams } from "react-router-dom";
 import Navbar from "../includes/Navbar";
 import Footer from "../includes/Footer";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getSingleProduct } from "../../store/singleProduct/singleProductSlice";
 import { Link } from "react-router-dom";
 import { addWishlistProduct, deleteWishlistProduct, getWishlist } from "../../store/wishlist/wishlistSlice";
+import { addCartItem, deleteCartItem, getCartItems } from "../../store/cart/cartSlice";
 
 function SingleProduct (){
 
     const params = useParams();
     const productSlug = params.productSlug
+    const [cartObject, setCartObject] = useState({"inCart":false,
+                                                   "cartItemId":null})
+    
     const {warehouse, isLoading} = useSelector((store)=>store.selectedWarehouse)
     const {singleProduct, isSingleProductLoading} = useSelector((store)=>store.singleProduct)
     const {wishlistProducts,isWishlistLoading} = useSelector((store)=>store.wishlist)
+    const {cartItems, isCartLoading} = useSelector((store)=>store.cart)
+
+    const imageUrl = `http://127.0.0.1:8000${singleProduct.product.image}`
 
     const dispatch = useDispatch()
 
     useEffect(()=>{
         dispatch(getSingleProduct(productSlug))
-    },[warehouse, productSlug])
+    },[warehouse, productSlug, dispatch])
 
+    useEffect(()=>{
+        dispatch(getCartItems())
+    },[warehouse,dispatch, productSlug])
+
+    useEffect(()=>{
+       const cartItem =  cartItems.filter((item)=>{return item.warehouse_product.id === singleProduct.id})[0]
+       if(cartItem){
+        setCartObject({"inCart":true,
+                        "cartItemId":cartItem.id})
+       } else{
+        setCartObject({"inCart":false,
+                        "cartItemId":null})
+       }
+
+    },[warehouse,singleProduct, cartItems,dispatch])
 
     const wishlistProduct = wishlistProducts.filter((wishlistProduct)=>{
                                 return wishlistProduct.warehouse_product.id === singleProduct.id 
@@ -37,6 +59,25 @@ function SingleProduct (){
     const deleteFromWishlistHandler =  (e) =>{
         e.preventDefault()
         dispatch(deleteWishlistProduct(wishlistProduct.id))
+    }
+
+    const deleteFromCartHandler =async (e) =>{
+        e.preventDefault()
+        await Promise.all([
+            dispatch(deleteCartItem(cartObject.cartItemId))
+        ]);
+        return dispatch(getCartItems());
+    }
+
+
+    const addToCartHandler = async(e) =>{
+        e.preventDefault()
+        
+        await Promise.all([
+            dispatch(addCartItem(singleProduct.id))
+        ]);
+        return dispatch(getCartItems());
+
     }
 
     return (
@@ -66,34 +107,7 @@ function SingleProduct (){
                                     <div className="product-dt-view">
                                         <div className="row">
                                             <div className="col-lg-4 col-md-4">
-                                                <div id="sync1" className="owl-carousel owl-theme">
-                                                    <div className="item">
-                                                        <img src="images/product/big-1.jpg" alt=""/>
-                                                    </div>
-                                                    <div className="item">
-                                                        <img src="images/product/big-2.jpg" alt=""/>
-                                                    </div>
-                                                    <div className="item">
-                                                        <img src="images/product/big-3.jpg" alt=""/>
-                                                    </div>
-                                                    <div className="item">
-                                                        <img src="images/product/big-4.jpg" alt=""/>
-                                                    </div>
-                                                </div>
-                                                <div id="sync2" className="owl-carousel owl-theme">
-                                                    <div className="item">
-                                                        <img src="images/product/big-1.jpg" alt=""/>
-                                                    </div>
-                                                    <div className="item">
-                                                        <img src="images/product/big-2.jpg" alt=""/>
-                                                    </div>
-                                                    <div className="item">
-                                                        <img src="images/product/big-3.jpg" alt=""/>
-                                                    </div>
-                                                    <div className="item">
-                                                        <img src="images/product/big-4.jpg" alt=""/>
-                                                    </div>
-                                                </div>
+                                                <img src={imageUrl} width="380" height="380" alt=""/>
                                             </div>
                                             <div className="col-lg-8 col-md-8">
                                                 <div className="product-dt-right">
@@ -108,27 +122,17 @@ function SingleProduct (){
                                                         <ul>
                                                             {singleProduct.discount_rate>0 &&  <li><div className="main-price color-discount">Discount Price<span>${singleProduct.get_discounted_price}</span></div></li>}
                                                             {singleProduct.discount_rate>0 &&  <li><div className="main-price mrp-price">MRP Price<span>${singleProduct.price}</span></div></li>}
-                                                            {!singleProduct.discount_rate>0 && <li><div className="main-price color-discount">MRP Price<span>${singleProduct.price}</span></div></li>}
+                                                            {!singleProduct.discount_rate>0 && <li><div className="main-price "> Price<span>${singleProduct.price}</span></div></li>}
                                                         </ul>
                                                         <ul className="gty-wish-share">
                                                             <li>
-                                                                <div className="qty-product">
-                                                                    <div className="quantity buttons_added">
-                                                                        {/* <input type="button" value="-" className="minus minus-btn"/>
-                                                                        <input type="number" step="1" name="quantity" value="1" className="input-text qty text"/>
-                                                                        <input type="button" value="+" className="plus plus-btn"/> */}
-                                                                    </div>
-                                                                </div>
-                                                            </li>
-                                                            <li>
                                                                 {!isWishlistLoading &&  !inWishlist && <span className="like-icon save-icon " title="wishlist" onClick={(e)=>addToWishlistHandler(e)} ></span>}
-                                                                {!isWishlistLoading &&   inWishlist && <span className="like-icon save-icon liked" title="wishlist" onClick={(e)=>deleteFromWishlistHandler(e)}></span>}
-                                                                
+                                                                {!isWishlistLoading &&   inWishlist && <span className="like-icon save-icon liked" title="wishlist" onClick={(e)=>deleteFromWishlistHandler(e)}></span>}  
                                                             </li>
                                                         </ul>
                                                         <ul className="ordr-crt-share">
-                                                            <li><button className="add-cart-btn hover-btn"><i className="uil uil-shopping-cart-alt"></i>Add to Cart</button></li>
-                                                            <li><button className="order-btn hover-btn">Order Now</button></li>
+                                                             {cartObject.inCart && <li><button className="add-cart-btn hover-btn"  onClick={(e)=>deleteFromCartHandler(e)} ><i className="uil uil-shopping-cart-alt"></i>Remove from Cart</button></li>}
+                                                             {!cartObject.inCart  && <li><button className="add-cart-btn hover-btn" onClick={(e)=>addToCartHandler(e)}><i className="uil uil-shopping-cart-alt"></i>Add to Cart</button></li>}
                                                         </ul>
                                                     </div>
                                                     <div className="pdp-details">
